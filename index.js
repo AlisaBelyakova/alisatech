82fe55180b696e46ab29bcaa4adc0a8bc79b9fa8
AlisaBelyakova <6183112@gmail.com>
2018-07-15 16:31:33 -0700

    apply transform scale for media and project pics, prevent overflow

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 window.addEventListener('load', () => {
     const aboutText = `Hi, I'm Alisa! <br> 
-    I am a full-stack software engineer with experience in startup ventures, 
-    public-private partnerships and Smart City applications. <br> 
-    My expertise is in Node.js, Express, React, Solidity, and Relational Database solutions.  
+    I'm a full-stack Software Engineer with experience in 
+    Node.js, Express, React, and Relational Database solutions. 
     <br> See my projects below <br> and drop me a line to say - HELLO :]`
+
+    // const aboutText = `Hi, I'm Alisa! <br> 
+    // I am a full-stack software engineer with experience in startup ventures, 
+    // public-private partnerships and Smart City applications. <br> 
+    // My expertise is in Node.js, Express, React, Solidity, and Relational Database solutions.  
+    // <br> See my projects below <br> and drop me a line to say - HELLO :]`
+
+
 
     const about = document.getElementById('about-text');
     let count = 0;
@@ -19,7 +26,8 @@ window.addEventListener('load', () => {
 
     let medias = Array.from(document.getElementsByClassName('border'));
     medias.forEach(item => item.addEventListener('mouseenter', (event) => {
-        event.target.style.height = window.innerWidth > 800 ? '5.5vw' : '16vw';
+        // event.target.style.height = window.innerWidth > 800 ? '5.5vw' : '16vw';
+        // event.target.style.transform = scale(1.1, 1.1)
 
         let prevImg = event.target.getAttribute('src').split('images/')[1];
         console.log(event.target.getAttribute('src'))
@@ -28,7 +36,8 @@ window.addEventListener('load', () => {
     }))
 
     medias.forEach(item => item.addEventListener('mouseleave', (event) => {
-        event.target.style.height = window.innerWidth > 800 ? '5vw' : '15vw';
+        // event.target.style.height = window.innerWidth > 800 ? '5vw' : '15vw';
+        // event.target.style.transform = scale(1, 1)
 
         let prevImg = event.target.getAttribute('src').split('images/')[1];
         event.target.setAttribute('src', './images/' + prevImg.slice(0, prevImg.indexOf('-white')) + '.png');
@@ -68,15 +77,14 @@ window.addEventListener('load', () => {
         }
 
 
-        let portfolio = document.getElementById('portfolio');
-        let portTop = portfolio.getBoundingClientRect().top;
-        // console.log('getBoundingClientRect', portTop);
+        // let portfolio = document.getElementById('portfolio');
+        // let portTop = portfolio.getBoundingClientRect().top;
 
-        if (scrollPosition > portTop - 100 && window.innerWidth > 800) {
-            Array.from(document.querySelectorAll('.project-img, .project-name')).forEach(item => {
-                item.style.opacity = '1';
-            })
-        }
+        // if (scrollPosition > portTop - 100 && window.innerWidth > 800) {
+        //     Array.from(document.querySelectorAll('.project-img, .project-name')).forEach(item => {
+        //         item.style.opacity = '1';
+        //     })
+        // }
 
         // document.querySelectorAll('.project-img').forEach(item => {
         //     item.style.rotation = '10deg';
