e05253b85de9516fc224c351998d106394cfcf7b
AlisaBelyakova <6183112@gmail.com>
2018-09-03 21:22:26 -0700

    introduction update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 window.addEventListener('load', () => {
     const aboutText = `Hi, I'm Alisa! <br> 
-    I'm hacking with Javascript and building the future in Web VR. 
-    I love solving challenging puzzles and team with people from various fields.   
+    I'm hacking with Javascript and building the future in Web VR ◎_◎
+    <br> I love solving challenging puzzles and team with people from various fields.   
     <br> See my projects below <br> and drop me a line to say - HELLO :]`
 
     const about = document.getElementById('about-text');
